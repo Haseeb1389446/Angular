a0b0af5f508c67d0c71c4d8e6635c893f9a33377
Haseeb1389446 <haseeb1389446@gmail.com>
2025-02-15 03:18:02 -0800

    Angular

---
+++ ng-Api-Consuming/src/app/getusers/getusers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-getusers',
+  imports: [],
+  templateUrl: './getusers.component.html',
+  styleUrl: './getusers.component.css'
+})
+export class GetusersComponent {
+
+}
